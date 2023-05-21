34695d0c6143215774e8bfdb036ae808c4be9ca7
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2023-05-21 03:29:55 -0400

    cambio de ruta

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use(cors(corsOptions));
 app.use("/api/usuarios", usuarios)
 
 // Ruta para entregar archivos estáticos
-app.use('/api/cdn', express.static('public'));
+app.use('/api/cdn', express.static('public/xss.js'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
